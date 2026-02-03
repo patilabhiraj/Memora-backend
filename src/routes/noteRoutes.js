b4f0c9053889b89d4a49b1bfdeb2c3237bd0d4d0
patilabhiraj <90681916+patilabhiraj@users.noreply.github.com>
2026-02-03 12:39:52 +0530

    face one ready like auth, crud,and onther feature

--- src/routes/noteRoutes.js
+++ src/routes/noteRoutes.js
@@ -34,4 +34,85 @@ router.get('/', authMiddleware, async (req, res) => {
   });
 });
 
+
+
+// // ================= UPDATE NOTE =================
+router.put('/:id', authMiddleware, async (req, res) => {
+  try {
+    const noteId = req.params.id;
+
+    // 1️⃣ FIND NOTE FIRST
+    const note = await Note.findById(noteId);
+
+    if (!note) {
+      return res.status(404).json({
+        message: 'Note not found',
+      });
+    }
+
+    // 2️⃣ CHECK OWNERSHIP
+    if (note.user.toString() !== req.userId) {
+      return res.status(403).json({
+        message: 'You are not allowed to update this note',
+      });
+    }
+
+    // 3️⃣ UPDATE FIELDS
+    const { title, content, tag, isPinned } = req.body;
+
+    if (title !== undefined) note.title = title;
+    if (content !== undefined) note.content = content;
+    if (tag !== undefined) note.tag = tag;
+    if (isPinned !== undefined) note.isPinned = isPinned;
+
+    // 4️⃣ SAVE
+    const updatedNote = await note.save();
+
+    res.status(200).json({
+      message: 'Note updated successfully',
+      note: updatedNote,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Server error',
+    });
+  }
+});
+
+// ================= Delete NOTE =================
+router.delete('/:id', authMiddleware, async (req, res) => {
+  try {
+    const noteId = req.params.id;
+
+    // 1️⃣ find note
+    const note = await Note.findById(noteId);
+
+    if (!note) {
+      return res.status(404).json({
+        message: 'Note not found',
+      });
+    }
+
+    // 2️⃣ ownership check
+    if (note.user.toString() !== req.userId) {
+      return res.status(403).json({
+        message: 'You are not allowed to delete this note',
+      });
+    }
+
+    // 3️⃣ delete note
+    await note.deleteOne();
+
+    res.status(200).json({
+      message: 'Note deleted successfully',
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'Server error',
+    });
+  }
+});
+
 module.exports = router;
